faf94118b8b7cd0f712432b680438b80a9b6caf5
AartiVerma10 <74553577+AartiVerma10@users.noreply.github.com>
2021-01-31 23:33:07 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,27 @@
+class Chain{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.5            
+        }
+
+       
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+   
+   show(){
+   
+        
+     
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.bodyB.position;
+                
+                stroke("black");
+                strokeWeight(7);
+                line(pointA.x , pointA.y, pointB.x , pointB.y);
+               
+    }
+    
+}
